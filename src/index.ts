5ed44460d3011d9ca73e220e98a5c23ff885aef8
Henry Chon 2 <chonhenry@outlook.com>
2022-07-17 18:01:51 -0400

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-import { Observable } from 'rxjs';
+import { Observable, map } from "rxjs";
 
-const someObservable$ = new Observable<string>(subscriber => {
-  subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
-  subscriber.complete();
+const someObservable$ = new Observable<number>((subscriber) => {
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+  subscriber.next(4);
+  subscriber.next(5);
+  subscriber.next(6);
+  subscriber.next(7);
+  subscriber.next(8);
+  subscriber.next(9);
+  subscriber.next(10);
 });
 
-someObservable$.subscribe(value => console.log(value));
+const timesTen$ = someObservable$.pipe(map((number) => number * 10));
+
+timesTen$.subscribe((value) => console.log(value));
