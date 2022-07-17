291b3c7fa5d0b160b6964500d310cd7e23131fbc
Henry Chon 2 <chonhenry@outlook.com>
2022-07-17 17:55:35 -0400

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-import { Observable } from 'rxjs';
+import { Observable, filter } from "rxjs";
 
-const someObservable$ = new Observable<string>(subscriber => {
-  subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
-  subscriber.complete();
+interface NewsItem {
+  category: "Business" | "Sports";
+  content: string;
+}
+
+const newsFeed$ = new Observable<NewsItem>((subscriber) => {
+  setTimeout(
+    () => subscriber.next({ category: "Business", content: "A" }),
+    1000
+  );
+  setTimeout(() => subscriber.next({ category: "Sports", content: "B" }), 3000);
+  setTimeout(
+    () => subscriber.next({ category: "Business", content: "C" }),
+    4000
+  );
+  setTimeout(() => subscriber.next({ category: "Sports", content: "D" }), 6000);
+  setTimeout(
+    () => subscriber.next({ category: "Business", content: "E" }),
+    7000
+  );
 });
 
-someObservable$.subscribe(value => console.log(value));
+const sportsNewsFeed$ = newsFeed$.pipe(
+  filter((item) => item.category === "Sports")
+);
+
+sportsNewsFeed$.subscribe((item) => console.log(item));
