afd6d1e5229340b56dfb964504b042a14052c942
Henry Chon 2 <chonhenry@outlook.com>
2022-07-17 16:57:28 -0400

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
+import { ajax } from "rxjs/ajax";
 
-const someObservable$ = new Observable<string>(subscriber => {
-  subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
-  subscriber.complete();
+// cold observable
+const ajax$ = ajax<any>("https://random-data-api.com/api/name/random_name");
+
+ajax$.subscribe((data) => console.log("Sub 1:", data.response.first_name));
+ajax$.subscribe((data) => console.log("Sub 2:", data.response.first_name));
+ajax$.subscribe((data) => console.log("Sub 3:", data.response.first_name));
+
+// hot observable
+const helloButton = document.querySelector("button");
+
+const helloClick$ = new Observable<MouseEvent>((subscriber) => {
+  helloButton.addEventListener("click", (event) => {
+    subscriber.next(event);
+  });
 });
 
-someObservable$.subscribe(value => console.log(value));
+helloClick$.subscribe((event) =>
+  console.log("Sub 1:", event.type, event.x, event.y)
+);
+
+setTimeout(() => {
+  console.log("Subscription 2 starts");
+  helloClick$.subscribe((event) => {
+    console.log("Sub 2:", event.type, event.x, event.y);
+  });
+}, 5000);
