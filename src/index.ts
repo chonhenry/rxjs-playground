debf62e81aef3ef8e4df167ba02bc36240aa06c0
Henry Chon 2 <chonhenry@outlook.com>
2022-07-18 23:06:47 -0400

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-import { Observable } from 'rxjs';
+import { Observable, catchError, of, EMPTY } from "rxjs";
 
-const someObservable$ = new Observable<string>(subscriber => {
-  subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
-  subscriber.complete();
+const failingHttpRequest$ = new Observable((subscriber) => {
+  setTimeout(() => {
+    subscriber.error(new Error("Timeout"));
+  }, 3000);
 });
 
-someObservable$.subscribe(value => console.log(value));
+console.log("App started");
+
+// failingHttpRequest$
+//   .pipe(catchError((error) => of("Fallback value")))
+//   .subscribe((value) => console.log(value));
+
+failingHttpRequest$
+  .pipe(catchError((error) => EMPTY))
+  .subscribe({
+    next: (value) => console.log(value),
+    complete: () => console.log("completed"),
+  });
